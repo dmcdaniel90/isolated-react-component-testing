02ab04c054d04ba828e3a4cae4556f03355ffbca
dmcdaniel90 <dmcdaniel9@gmail.com>
2024-02-23 12:38:57 +0000

    updated Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const preview: Preview = {
+  args: {onClick: fn()},
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
